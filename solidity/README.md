7d85b162064e59d34af42037f185f8f4dcb439a9
Frances He <hesiyun1996@gmail.com>
2022-05-18 02:47:02 +0800

    add: more solidity syntax, function modifier

--- solidity/README.md
+++ solidity/README.md
@@ -48,3 +48,29 @@ Following convention, constants should be named with all capital letters with un
 
 THIS_IS_MY_CONSTANT_VAR
 
+* Tenary operator:
+y = x > 1 ? 10 : 20;
+
+* Error Handling
+"""
+Solidity has 3 ways to throw an error, require, revert and assert.
+
+require is used to validate inputs and check conditions before and after execution.
+revert is like require but more handy when the condition to check is nested in several if statements.
+assert is used to check invariants, code that should never be false. Failing assertion probably means that there is a bug.
+An error will undo all changes made during a transaction.
+"""
+
+
+
+* Function modifier: allow you to reuse code
+"""
+Function modifiers are reuseable code that can be run before and / or after a function call.
+
+Here are some examples of how they are used.
+
+Restrict access
+Validate inputs
+Check states right before and after a function call
+"""
+
---
+++ solidity/smart_contract_engineer_challenges/function_modifier.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract FunctionModifier {
+    bool public paused;
+    uint public count;
+
+    // Modifire to check if not paused
+    modifier whenNotPaused() {
+        require(!paused, "paused");
+        // Underscore is a special character only used inside
+        // a function modifier and it tells Solidity to
+        // execute the rest of the code.
+        _;
+    }
+
+    function setPause(bool _paused) external {
+        paused = _paused;
+    }
+
+    // This function will throw an error if paused
+    function inc() external whenNotPaused {
+        count += 1;
+    }
+    
+    /*Task1:
+    Add the modifier whenNotPaused to dec() so that 
+    count can only be decreased if paused is false.
+    */
+    function dec() external whenNotPaused{
+        count -= 1;
+        
+    }
+    
+    
+    /*Task2:
+    Create a modifier named whenPaused, which checks that 
+    the state variable pause is true.
+    It should throw an error message "not paused" if paused 
+    is false.
+    */
+    modifier whenPaused(){
+        if(!paused){
+            revert("not paused");
+        }
+        // execute the rest of the code.
+        _;
+    }
+    
+    
+    // Modifiers can take inputs.
+    // Here is an example to check that x is < 10
+    modifier cap(uint _x) {
+        require(_x < 10, "x >= 10");
+        _;
+    }
+
+    function incBy(uint _x) external whenNotPaused cap(_x) {
+        count += _x;
+    }
+
+    // Modifiers can execute code before and after the function.
+    modifier sandwich() {
+        // code here
+        _;
+        // more code here
+    }
+    
+    //Task3:
+    function reset() external whenPaused {
+        count = 0;
+    }
+    
+}
